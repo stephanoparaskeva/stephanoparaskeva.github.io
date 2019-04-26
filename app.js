51a4a9abd9bfb364b4fa255e5c8b390eea88d844
Stephano Paraskeva <stephanokparaskeva@gmail.com>
2019-04-26 14:03:46 +0100

    added plotly sunburst chart

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-var data = [{
-    type: "sunburst",
-   labels: ["TECH", "Front-end", "JavaScript ES5/ES6", "React", "jQuery", "Back-end", "Python", "Node.js"],
-  parents: ["", "TECH", "Front-end", "Front-end", "Front-end","TECH", "Back-end", "Back-end"],
-    values:  [10, 14, 12, 10, 2, 6, 6, 4],
-    outsidetextfont: {size: 20, color: "#377eb8"},
-    leaf: {opacity: 0.4},
-    marker: {line: {width: 2}},
-  }];
+document.addEventListener('DOMContentLoaded', () => {
   
-  var layout = {
-    margin: {l: 0, r: 0, b: 0, t: 0},
-    width: 500,
-    height: 500
-  };
-  
-  
-  Plotly.newPlot('myDiv', data, layout, {showSendToCloud:true});
+  sunburst = document.getElementById('tester')
+
+  const data = [{
+      type: "sunburst",
+     labels: ["TECH", "Front-end", "JavaScript ES5/ES6", "React", "jQuery", "Back-end", "Python", "Node.js"],
+    parents: ["", "TECH", "Front-end", "Front-end", "Front-end","TECH", "Back-end", "Back-end"],
+      values:  [10, 14, 12, 10, 2, 6, 6, 4],
+      outsidetextfont: {size: 20, color: "#377eb8"},
+      leaf: {opacity: 0.4},
+      marker: {line: {width: 2}},
+    }];
+    
+    const layout = {
+      margin: {l: 0, r: 0, b: 0, t: 0},
+      width: 500,
+      height: 500
+    };
+    
+    
+    Plotly.newPlot('sunburst', data, layout, {showSendToCloud:true})
+
+
+})
